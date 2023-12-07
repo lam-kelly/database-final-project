e35b3dc40bd2239f655404917a80c51ad1ba0edb
Kelly Lam <lamtkelly95@gmail.com>
2023-12-07 00:27:31 -0500

    linear regression for airlines dataset

--- linear-regression.py
+++ linear-regression.py
@@ -1,38 +1,51 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import pandas as pd
-
-df = pd.read_csv('datasheet.csv')
-df.info()
-df.shape
-df.isnull().sum()
-print(df)
-
-#Separate the null values from dataframe (df) and create a variable “test data”
-test_data = df[df['C'].isnull()]
-
-#Drop the null values from the dataframe (df) and represent as ‘train data”
-df.dropna(inplace=True)
-
-
-#Create “x_train” & “y_train” from train data.
-x_train = df.drop('C',axis=1)
-y_train = df['C']
-
-
-#Build the linear regression model
+import numpy as np
+from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
-lr = LinearRegression()
-lr.fit(x_train, y_train)
 
+def linear_regression_imputation(missing_percent):
+    df = pd.read_csv("airlines.csv")
 
-x_test = test_data[['A','B']]
+    # delete 10% of specified column
+    df.loc[df.sample(frac=missing_percent).index, "time"] = np.nan
 
+    # Drop flight column since it has identifier that shouldn't affect other columns
+    df = df.drop(['flight'], axis=1)
 
-#Apply the model on x_test of test data to make predictions.
-y_pred = lr.predict(x_test)
+    # Nne hot encode categorical values
+    cat_variables = df[['airline', 'airportfrom', 'airportto']]
+    cat_dummies = pd.get_dummies(cat_variables, drop_first=True)
+    # Replace categorical columns with one-hot encoded columns
+    df = df.drop(['airline', 'airportfrom', 'airportto'], axis=1)
+    df = pd.concat([df, cat_dummies], axis=1)
 
-#Replace the missing values with predicted values.
-test_data['y_pred'] = y_pred
-print(test_data)
+    # Separate the null value rows from dataframe (df) and create a variable “test data”
+    test_data = df[df['time'].isnull()]
+
+    # Drop the null values from the dataframe (df) and represent as ‘train data”
+    df.dropna(inplace=True)
+
+    # Use all other columns to predict time
+    x_train = df.drop(['time'], axis=1)
+    x_test = test_data.drop(['time'], axis=1)
+    # target variable is time
+    y_train = df['time']
+    y_test = test_data['time']
+
+    # Build the linear regression model
+    lr = LinearRegression()
+    lr.fit(x_train, y_train)
+
+    # Apply the model on x_test of test data to make predictions.
+    y_pred = lr.predict(x_test)
+
+    # Get the correct answers from original dataset
+    original = pd.read_csv("airlines.csv")
+    original = original.loc[y_test.index, "time"]
+
+    # root mean squared error
+    rms = np.sqrt(mean_squared_error(original, y_pred))
+    print(rms)
+
+# eventually, put this in for-loop for various missing percentages
+linear_regression_imputation(.01)
