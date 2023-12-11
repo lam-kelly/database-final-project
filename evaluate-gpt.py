c9fecc931cfbf3e9da2fd23cfdd532083663ecec
Kelly Lam <lamtkelly95@gmail.com>
2023-12-10 23:13:09 -0500

    Add gpt results for mcar

---
+++ evaluate-gpt.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.linear_model import LinearRegression
+from math import sqrt
+
+def evaluate(actualCSV, testCSV):
+    actual = pd.read_csv("time_MCAR/actual/time_MCAR_1_actual.csv")
+    pred = pd.read_csv("time_MCAR_30.0_1_imputed.csv")
+    df_test = pd.read_csv("time_MCAR/test/time_MCAR_30.0_1.csv")
+
+    test_data = df_test[df_test['time'].isnull()]
+    y_test = test_data['time']
+    print(y_test)
+
+    # Get the correct answers from original dataset
+    actual = actual.loc[y_test.index, "time"]
+    y_pred = pred.loc[y_test.index, "time"]
+
+    # root mean squared error
+    mrse = sqrt(mean_squared_error(actual, y_pred))
+    print("MRSE", mrse)
+
+    mae = mean_absolute_error(actual, y_pred)
+    print("MAE", mae)
+    return mrse, mae
+
+evaluate("","")
