cd06daa38570c4673a83ff0d4e62fbffbf1ae503
Kelly Lam <lamtkelly95@gmail.com>
2023-12-11 18:45:04 -0500

    Results for remaining missingness patterns

--- experiments.py
+++ experiments.py
@@ -4,13 +4,12 @@ from KnnImputerAirline import knn_imputation_time, knn_imputation_airportfrom
 import os
 import json
 
-actual_files_time = ["/time_MCAR/time_MCAR_0.1_1_answer.csv", "time_MCAR_0.1_1_actual.csv"]
-test_files_time = ["time_MCAR_0.1_1_answer.csv", "time_MCAR_0.1_1_actual.csv"]
 result = {'experiments': []}
 
-# for test_file in os.listdir("time_MCAR/test"):
-#     test_file_name = "time_MCAR/test/" + test_file
-#     actual_file = "time_MCAR/actual/time_MCAR_" + str(test_file[-5]) + "_actual.csv"
+# for test_file in os.listdir("time_MNAR/test"):
+#     test_file_name = "time_MNAR/test/" + test_file
+#     actual_file = "time_MNAR/actual/time_MNAR_" + str(test_file[-5]) + "_actual.csv"
+#     print(test_file_name)
 #     imputations = {"testfile": test_file, 'actual_file': actual_file, 'imputations': []}
 #     mrse, mae = mean_imputation(actual_file, test_file_name)
 #     imputations["imputations"].append({"imputation": 'mean', 'MRSE': mrse, 'MAE': mae})
@@ -20,15 +19,13 @@ result = {'experiments': []}
 #     imputations["imputations"].append({"imputation": 'KNN', 'MRSE': mrse, 'MAE': mae})
 #     result['experiments'].append(imputations)
 
-# with open('results.json', 'w') as fp:
+# with open('time_MNAR_results.json', 'w') as fp:
 #     json.dump(result, fp)
 
-# actual_files_airportfrom = ["time_MCAR_0.1_1_answer.csv", "time_MCAR_0.1_1_actual.csv"]
-# test_files_airportfrom = ["time_MCAR_0.1_1_answer.csv", "time_MCAR_0.1_1_actual.csv"]
 
-for test_file in os.listdir("airportfrom_MCAR/test"):
-    test_file_name = "airportfrom_MCAR/test/" + test_file
-    actual_file = "airportfrom_MCAR/actual/airportfrom_MCAR_" + str(test_file[-5]) + "_actual.csv"
+for test_file in os.listdir("airportfrom_MNAR/test"):
+    test_file_name = "airportfrom_MNAR/test/" + test_file
+    actual_file = "airportfrom_MNAR/actual/airportfrom_MNAR_" + str(test_file[-5]) + "_actual.csv"
     imputations = {"testfile": test_file, 'actual_file': actual_file, 'imputations': []}
     accuracy, precision, recall, f1_score = mode_imputation(actual_file, test_file_name)
     imputations["imputations"].append({"imputation": 'mode', 'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1_score': f1_score})
@@ -36,5 +33,5 @@ for test_file in os.listdir("airportfrom_MCAR/test"):
     imputations["imputations"].append({"imputation": 'KNN', 'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1_score': f1_score})
     result['experiments'].append(imputations)
 
-with open('results.json', 'w') as fp:
+with open('airportfrom_MNAR_results.json', 'w') as fp:
     json.dump(result, fp)
